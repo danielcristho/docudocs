26369bc3d0e1b2f22110555f8dcbb042134f96c7
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-07-08 14:16:13 +0700

    feat: add search plugin

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,7 +4,7 @@ import Layout from "@theme/Layout";
 import AvatarImageUrl from "@site/static/img/me.jpg";
 import styles from "./styles.module.css";
 import Translate, { translate } from "@docusaurus/Translate";
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
 
 
 const { Head } = require('@docusaurus/Head');
@@ -15,14 +15,14 @@ function About() {
 
   return (
     <Layout title="About me" description={siteConfig.tagline}>
-      <Helmet>
+      {/* <Helmet> */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
           integrity="sha512-rb6W21CQz0AmTjEEBhHOtA9u9Ux7+cCViCXcHEDBsu1RzQ0RETN7/XAYCCt+T72M+ZwO2bzM6aaZIYODewYD+A=="
           crossorigin="anonymous"
         />
-      </Helmet>
+      {/* </Helmet> */}
       <header className={styles.aboutHeader}>
         <h2 className="underlineColorSuccess">
           <Translate id="about.aboutMeTittle"/>
@@ -51,15 +51,6 @@ function About() {
               <a href="https://www.linkedin.com/in/daniel-pepuho" target="_blank"> Daniel Pepuho</a>.
             </p>
             <p>Thanks for stopping by.</p>
-            {/* <a href="https://stackoverflow.com/users/12501050/m19v">
-              <img
-                src="https://stackexchange.com/users/flair/17263784.png"
-                width="208"
-                height="58"
-                alt="profile for m19v on Stack Overflow, a network of free, community-driven Q&amp;A sites"
-                title="profile for m19v on Stack Overflow, a network of free, community-driven Q&amp;A sites"
-              />
-            </a> */}
           </div>
         </div>
       </main>
