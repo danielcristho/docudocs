85a383650be7a83ce31f4fb24e5c0f19fe21ea44
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-07-12 02:41:21 +0700

    feat: update css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,15 +81,13 @@ const config = {
     {
       href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
       type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
     },
     {
       href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
       type: 'text/css',
     },
   ],
+
   scripts: [
     {
       src: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js',
